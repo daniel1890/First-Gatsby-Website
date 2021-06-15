0b386a051e19a0b147cfdd40a120f29859587b4a
Daniël R <danielroth1890@gmail.com>
2021-06-16 01:32:57 +0200

    Finished tutorial part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
-    title: "First Gatsby Site",
+    title: 'First Gatsby Site',
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,20 @@
 // Step 1: Import React
-import * as React from 'react'
-import Layout from '../components/layout'
+import * as React from 'react';
+import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
+    <Layout pageTitle='Home Page'>
       <p>I'm making this by following the Gatsby Tutorial.</p>
-      </Layout>
-  )
-}
+      <StaticImage
+        alt='Cute Kitties Sitting in a Row'
+        src='https://images2.fanpop.com/image/photos/10500000/Pretty-Kitty-Wallpaper-cats-10547167-1600-1200.jpg'
+      />
+    </Layout>
+  );
+};
 
 // Step 3: Export your component
-export default IndexPage
+export default IndexPage;
