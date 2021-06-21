5b1b27167a412e9d8e2d429b40288011484e6a13
daniel1890 <danielroth1890@gmail.com>
2021-06-21 15:43:11 +0200

    finished gatsby tutorial part 4!

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: 'First Gatsby Site',
+    description: 'A beautiful website filled with kitties'
   },
   plugins: [
     'gatsby-plugin-gatsby-cloud',
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blogposts`,
+      }
+    },
   ],
 };
